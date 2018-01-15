20b7111be682cf4d4e6d33bfdb568d044b5b9888
Drake Fish <drake_fish@yahoo.com>
2018-01-15 15:04:40 -0600

    committing

--- client/components/Grid.js
+++ client/components/Grid.js
@@ -6,17 +6,43 @@ import Pages from './Pages';
 class Grid extends React.Component {
     constructor(props) {
           super(props);
+      this.state={
+                  clicked:false,
+                  reviewText:'',
+                  reviewId:''
+                  }
     this.handlePage=this.handlePage.bind(this);
+    this.clickHandler=this.clickHandler.bind(this);
+    this.closeReview=this.closeReview.bind(this);
     }
 
-handlePage(i){
-  console.log("clicked",i);
-  this.props.pagination(i);
- }
+  handlePage(i){
+    console.log("clicked",i);
+    this.props.pagination(i);
+   }
+  clickHandler(reviewText,reviewId){
+    this.setState({clicked:true,reviewText:reviewText, reviewId:reviewId});
+  }
+  closeReview(){
+  this.setState({clicked:false, reviewText:'', reviewId:''});
+  }
 
 render(){
   let Items;
-
+  let reviewText;
+  if(this.state.clicked){
+    reviewText=(
+                  <div onClick={this.closeReview} className="review-modal">
+                    <div className="review-text-container">
+                      <button onClick={this.closeReview} type="button" className="close" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                      <h5 className="review-id">ReviewID: {this.state.reviewId}</h5>
+                      <p className="review-text">{this.state.reviewText}</p>
+                    </div>
+                  </div>
+                );
+  }
   if(this.props.data.length>1){
 
   //create the pages
@@ -31,6 +57,7 @@ console.log("TOTAL RESULTS AT GRID", this.props.results);
 return(
       <div>
         <h1 id="displayableReviewsTitle">{this.props.title}</h1>
+        {reviewText}
         <table className="tableMain" id="displayableReviews">
         	<tbody>
         		<tr className="tableHeader">
