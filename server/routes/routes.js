1b6d7efd7794c098fd8cdb23c6b6dea286e4de03
Drake Fish <drake_fish@yahoo.com>
2018-03-21 15:51:37 -0500

    test

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -139,7 +139,7 @@ router.route('/syndicationDashboard').post(function(req,res){
             res.json(displayErr);
           }
           if(displayBody){
-            console.log('** SB Display this works just as expected!');
+            console.log('** SB Display this works just as expected! ');
             // iterate through syndication sources
             for(let i=0,len=source["syndication"].length;i<len;i++){
               var syndClient = source["syndication"][i][0];
